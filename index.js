5cbaae277cde66fccdcd280f744e3eebc6623f2e
Rafa Garrido <rafagarridolara@gmail.com>
2021-04-20 00:53:25 -0400

    Refact replaceTemplate

--- index.js
+++ index.js
@@ -2,21 +2,7 @@ const http = require("http");
 const fs = require("fs");
 const url = require("url");
 
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCT_NAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-
-  return output;
-};
+const replaceTemplate = require("./modules/replaceTemplate");
 
 const data = fs.readFileSync("./dev-data/data.json", "utf-8");
 const dataObj = JSON.parse(data);
